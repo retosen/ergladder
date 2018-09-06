d0a653956946a6f5a233f547145e37e19ce0dd57
retosen <34863491+retosen@users.noreply.github.com>
2018-09-07 00:02:04 +0100

    Complete Restart
    New user model was not defined at start. To make life easier and due to the very small amount of work done, I opted for a complete reset.

--- ladder/models.py
+++ ladder/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    date_of_birth = models.DateField()
+
+    REQUIRED_FIELDS = ['date_of_birth']
