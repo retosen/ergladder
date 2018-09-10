80284ae349370c539533245fbceeffdf2a94d3a9
retosen <34863491+retosen@users.noreply.github.com>
2018-09-10 16:29:56 +0100

    Altering Models

--- ergladder/urls.py
+++ ergladder/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('ladder/', include('ladder.urls')),
     path('admin/', admin.site.urls),
 ]
--- ladder/models.py
+++ ladder/models.py
@@ -3,5 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     date_of_birth = models.DateField()
+    email = models.EmailField(unique=True)
 
-    REQUIRED_FIELDS = ['date_of_birth']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth', 'username']
